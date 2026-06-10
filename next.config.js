e6a38a8bf546be5f3d4932829723b1b310445842
jiruejeta <jiruejeta@gmail.com>
2026-06-10 22:58:41 +0300

    Add CSP headers in next.config

--- next.config.js
+++ next.config.js
@@ -15,7 +15,19 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; connect-src * https://moe-36gy.onrender.com;",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
